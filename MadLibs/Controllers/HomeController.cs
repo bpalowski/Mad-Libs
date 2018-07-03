855431152d1b85618c3f03b39fcab08c82b45aef
Brian <b.scott9592@gmail.com>
2018-07-02 19:31:47 -0700

    Still working on

---
+++ MadLibs/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Lib.Models;
+
+namespace Lib.Controllers
+{
+  public class HomeController : Controller
+  {
+    [Route("/")]
+    //  public string Hello() { return "Hello friend!"; }
+    public ActionResult Form()
+    {
+      return View();
+    }
+
+    [Route("/MadLibs")]
+    //  public string Hello() { return "Hello friend!"; }
+    public ActionResult MadLibs()
+    {
+      Letter myLetter = new Letter();
+      myLetter.SetRecipient(Request.Query["recipient"]);
+      myLetter.SetSender(Request.Query["sender"]);
+      return View(myLetter);
+    }
+  }
+}
